030bb76facede52db1cf3ba6f0e1d8fbc4f5ed2b
Atul Srivastav <122530825+sriatul@users.noreply.github.com>
2024-02-09 12:38:57 +0530

    Angular Project

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+private url ='https://jsonplaceholder.typicode.com/posts';
+  constructor(private client:HttpClient) { }
+
+
+getPosts(){
+  return this.client.get(this.url)
+}
+
+}
